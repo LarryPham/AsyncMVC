f0a2c5d0ad2708cf159312116a1356d721867963
Larry Pham <larrypham.vn@gmail.com>
2014-11-11 12:00:50 +0700

    First Commit

---
+++ src/com/techiedb/app/asyncmvc/MainAsyncMvcActivity.java
@@ -0,0 +1,13 @@
+package com.techiedb.app.asyncmvc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainAsyncMvcActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
